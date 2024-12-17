bd43c779957603cfb612bf8d60b3e1afb1b88eef
Yash <yashshah@gmail.com>
2024-12-17 13:44:24 +0530

    UC1 Attendance added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,5 +1,14 @@
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program!");
-    } 
+
+        // UC-1
+        System.out.println("Checking whether Employee is present or absent");
+        int present = 1;
+        double employeeCheck = Math.floor(Math.random() * 10) % 2;
+        if (employeeCheck == present)
+            System.out.println("Employee is PRESENT");
+        else
+            System.out.println("Employee is ABSENT");
+    }
 }
