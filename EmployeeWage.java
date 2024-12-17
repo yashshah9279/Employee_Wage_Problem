a74ca349f150b8c29a4fff8783b11f33d5c6318a
Yash <yashshah@gmail.com>
2024-12-17 18:01:24 +0530

    UC2 Attendance added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,13 +2,21 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program!");
 
-        // UC-1
-        System.out.println("Checking whether Employee is present or absent");
-        int present = 1;
+        // UC-1 and UC-2
+        int isPresent = 1;
+        int wagePerHr = 20;
+        int empHrs, empWage;
         double employeeCheck = Math.floor(Math.random() * 10) % 2;
-        if (employeeCheck == present)
+        if(employeeCheck == isPresent){
             System.out.println("Employee is PRESENT");
-        else
+            empHrs = 8;
+        }
+        else {
             System.out.println("Employee is ABSENT");
+            empHrs = 0;
+        }
+        empWage = empHrs * wagePerHr;
+        System.out.println("Employee's Hours are: " + empHrs);
+        System.out.println("Employee's Wage is: " + empWage);
     }
 }
